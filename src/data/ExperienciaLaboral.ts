3d2def5f59619666d32e68b76dd760c5d466f217
Jul879n <jul.sitebeam@gmail.com>
2024-02-24 20:29:00 -0300

    el ancho de los elementos paso a ser w-7/12 en sm y w-full en móvil

---
+++ src/data/ExperienciaLaboral.ts
@@ -0,0 +1,18 @@
+export const ExperienciaLaboral = [
+    {
+        id: 'mi-solar',
+        empresa:'mi solar',
+        cargo:'desarrollador web',
+        fechaInicio:'2022',
+        fechaFin:'actualidad',
+        texto:'Desarrollé y mantuve el sitio web de la empresa utilizando HTML, CSS y JavaScript. Desarrollé una aplicación para la empresa en la cual generé calculadoras enfocadas en diversos ámbitos de la electricidad con JavaScript.'
+    },
+    {
+        id: 'zaimoku-taller',
+        empresa:'zaimoku taller',
+        cargo:'desarrollador web',
+        fechaInicio:'2022',
+        fechaFin:'2023',
+        texto:'Diseñé y desarrollé el sitio web de la empresa utilizando HTML y CSS. Implementé el seguimiento de estadísticas de tráfico y conversión utilizando Google Analytics'
+    },
+]
